a465a62b96ced937ed4332049688e4fb5aec8b7c
JeanZ <970263026@qq.com>
2012-08-20 17:33:12 +0800

    first commit

---
+++ src/energy/OrginfoHome.java
@@ -0,0 +1,123 @@
+package energy;
+
+// Generated 2012-8-20 16:24:44 by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Orginfo.
+ * @see energy.Orginfo
+ * @author Hibernate Tools
+ */
+public class OrginfoHome {
+
+	private static final Log log = LogFactory.getLog(OrginfoHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Orginfo transientInstance) {
+		log.debug("persisting Orginfo instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Orginfo instance) {
+		log.debug("attaching dirty Orginfo instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Orginfo instance) {
+		log.debug("attaching clean Orginfo instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Orginfo persistentInstance) {
+		log.debug("deleting Orginfo instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Orginfo merge(Orginfo detachedInstance) {
+		log.debug("merging Orginfo instance");
+		try {
+			Orginfo result = (Orginfo) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Orginfo findById(java.lang.String id) {
+		log.debug("getting Orginfo instance with id: " + id);
+		try {
+			Orginfo instance = (Orginfo) sessionFactory.getCurrentSession()
+					.get("energy.Orginfo", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Orginfo instance) {
+		log.debug("finding Orginfo instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("energy.Orginfo")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
